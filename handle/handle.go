69fa8d23affb8bdb5417df27846515db4298d69f
belyaevd <belyaevpilot@gmail.com>
2021-05-04 10:23:20 +0300

    handle add

---
+++ handle/handle.go
@@ -0,0 +1,12 @@
+package handle
+
+import (
+	"html/template"
+	"net/http"
+)
+
+var tpl = template.Must(template.ParseFiles("templates/index.html"))
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	tpl.Execute(w, nil)
+}
